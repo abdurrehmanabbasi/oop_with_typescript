5b6723f8a837bb83aa34b38595f4fc20c10db2ba
abdurrehman <abdurrehmanabbasi8@gmail.com>
2023-05-15 09:07:00 +0500

    backaccount problem part1

--- src/problems/bank.ts
+++ src/problems/bank.ts
@@ -1,5 +1,26 @@
 // Create a class representing a bank account with methods to withdraw, deposit and check balance. (Easy)
 
-class Bank{
-    
+class BankAccount {
+  public balance: number;
+  constructor(balance: number) {
+    this.balance = balance;
+  }
+  public deposit(amount: number){
+    this.balance += amount;
+    return this.balance
+  }
+  public withdraw(amount: number) :boolean{
+    if (amount>this.balance){
+        return false
+    }else{
+        this.balance -= amount;
+        return true
+    }
+  }
 }
+let newAccount=new BankAccount(100)
+console.log(newAccount.balance)
+newAccount.deposit(1000)
+console.log(newAccount.balance)
+newAccount.withdraw(1500)
+console.log(newAccount.balance)
